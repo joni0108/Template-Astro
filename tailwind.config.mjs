98db2946ea1010c4bd4aa5821ce31c5581155db1
joni0108 <trollpro0108@icloud.com>
2024-06-06 11:41:36 -0700

    DarkModeAdded

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: "class",
+    theme: {
+        extend: {
+            colors: {
+                font_light: "#111827",
+                font_dark: "#e5e7eb",
+                bg_light: "#f3f4f6",
+                bg_dark: "#111827",
+                primary: "#2563eb",
+                success: "#22c55e",
+                error: "#ef4444",
+            },
+        },
+    },
+    plugins: [],
 }
